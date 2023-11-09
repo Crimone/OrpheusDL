b95f5c8b32012c599bae0897ef7f5015a585130f
Crimone <411531507@qq.com>
2023-11-09 21:46:36 +0800

    modified:   utils/utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -24,7 +24,7 @@ sanitise_name = lambda name : re.sub(r'[:]', ' - ', re.sub(r'[\\/*?"<>|$]', '', 
 
 def fix_byte_limit(path: str, byte_limit=250):
     # only needs the relative path, the abspath uses already existing folders
-    rel_path = os.path.relpath(path).replace('\\', '/')
+    rel_path = os.path.abspath(path).replace('\\', '/')
 
     # split path into directory and filename
     directory, filename = os.path.split(rel_path)
